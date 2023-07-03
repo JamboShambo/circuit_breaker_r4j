be7417c2bea63154c0e660a53b98a56cbc64e185
JamboShambo <jamiedonohoe2805@gmail.com>
2023-07-03 21:26:47 +0100

    Initial commit

---
+++ src/main/java/com/example/circuit_breaker_r4j/CircuitBreakerR4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.circuit_breaker_r4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerR4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerR4jApplication.class, args);
+	}
+
+}
